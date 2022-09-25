486f1a8a9bc8251c106a0c8a2d714cc8390f3a25
Pedro Cardoso <pedrohfsc96@gmail.com>
2022-09-25 08:11:02 -0300

    quiz-app

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -47,6 +47,8 @@ const quizData = [
     
 
 ];
+const quiz = document.getElementById('quiz')
+const answersEl = document.querySelectorAll(".answer");
 const questionEl = document.getElementById('question')
 const a_text = document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
@@ -55,10 +57,13 @@ const d_text = document.getElementById('d_text');
 const submitBtn = document.getElementById('submit');
 
 let currentQuiz = 0;
+let score = 0;
+
 
 loadQuiz();
 
 function loadQuiz(){
+    deselectAnswer();
 
     const currentQuizData = quizData[currentQuiz];
 
@@ -70,16 +75,42 @@ function loadQuiz(){
 
 }
 
+function getSelected(){
+    
+
+    let answer = undefined;
+
+    answersEl.forEach((answersEl) => {
+        if(answersEl.checked){
+            answer = answersEl.id;
+        }
+    });
+    return answer;
+}
+
+function deselectAnswer(){
+    answersEl.forEach((answersEl) => {
+        answersEl.checked = false;
+    });
+}
+
 submitBtn.addEventListener('click', () => {
-    currentQuiz++;
+    
+    const answer = getSelected();
+    
+    if(answer){
 
-    if(currentQuiz < quizData.length){
-        loadQuiz();
-    } else {
-        alert("Você terminou o Quiz! Beba agua, hidrate-se.")
-    }
+        if(answer === quizData[currentQuiz].correct){
+            score++
+        }
+        currentQuiz++;
+        if(currentQuiz < quizData.length){
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `<h2>Você acertou ${score}/${quizData.length} questões!</h2>
+            <button onclick="location.reload()">Reiniciar</button>`
+        }
+        
+    } 
 
-
-    loadQuiz();
-
-})
+});
